cb104363d6bcb45f5253ca962e33342dfeac1749
Greg Morgan <gregm9999@gmail.com>
2018-12-14 23:34:36 +0000

    Done.

--- db/migrate/20181214201122_create_recipes.rb
+++ db/migrate/20181214201122_create_recipes.rb
@@ -2,8 +2,8 @@ class CreateRecipes < ActiveRecord::Migration
   def change
     create_table :recipes do |recipe|
       recipe.string :name
-      recipe.text :ingredients
-      recipe.decimal :cook_time
+      recipe.string :ingredients
+      recipe.string :cook_time
     end
   end
 end
