7c0bfaa2653c27d762c550fb3a59e09733777e9c
Greg Morgan <gregm9999@gmail.com>
2018-12-14 23:37:26 +0000

    Automatically backed up by Learn

--- db/migrate/20181214201122_create_recipes.rb
+++ db/migrate/20181214201122_create_recipes.rb
@@ -2,7 +2,11 @@ class CreateRecipes < ActiveRecord::Migration
   def change
     create_table :recipes do |recipe|
       recipe.string :name
+<<<<<<< HEAD
       recipe.string :ingredients
+=======
+      recipe.text :ingredients
+>>>>>>> ac7d251382479725e937d8e30a60cb85898e9896
       recipe.string :cook_time
     end
   end
